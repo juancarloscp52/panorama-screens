ec578a9bfa5c204117ddd159c28bfa800aeaa773
jc <juancarloscp52@gmail.com>
2023-04-22 16:54:29 +0200

    Remove duplicated code

--- common/src/main/java/me/juancarloscp52/panorama_screen/RotatingCubeMapRenderer.java
+++ common/src/main/java/me/juancarloscp52/panorama_screen/RotatingCubeMapRenderer.java
@@ -44,25 +44,18 @@ public class RotatingCubeMapRenderer {
     }
 
     public void render(float alpha, boolean titleScreen){
-        cubeMap = TitleScreen.CUBE_MAP;
         this.cubeMap.render(Minecraft.getInstance(), Mth.sin(time*0.001F)*5.0F + 25.0F,-this.time*0.1F,alpha);
-//        if(!titleScreen){
-//            PoseStack matrices = new PoseStack();
-//            RenderSystem.setShaderTexture(0,overlay);
-//            RenderSystem.enableBlend();
-//            RenderSystem.blendFunc(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA);
-//            RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, this.doBackgroundFade ? (float)Mth.ceil(Mth.clamp(alpha, 0.0F, 1.0F)) : 1.0F);
-//            Window window = Minecraft.getInstance().getWindow();
-//            GuiComponent.blit(matrices, 0, 0, window.getGuiScaledWidth(), window.getHeight(), 0.0F, 0.0F, 16, 128, 16, 128);
-//
-//            //Render panorama overlay
-//            RenderSystem.setShaderTexture(0, PANORAMA_OVERLAY);
-//            RenderSystem.enableBlend();
-//            float f = this.doBackgroundFade ? (float)(Util.getMillis() - this.backgroundFadeStart) / 1000.0f : 1.0f;
-//            RenderSystem.setShaderColor(1.0f, 1.0f, 1.0f, this.doBackgroundFade ? (float)Mth.ceil(Mth.clamp(f, 0.0f, 1.0f)) : 1.0f);
-//            TitleScreen.blit(matrices, 0, 0, Minecraft.getInstance().getWindow().getGuiScaledWidth(), Minecraft.getInstance().getWindow().getGuiScaledHeight(), 0.0f, 0.0f, 16, 128, 16, 128);
-//            RenderSystem.setShaderColor(1.0f, 1.0f, 1.0f, 1.0f);
-//        }
+        if(!titleScreen){
+            PoseStack matrices = new PoseStack();
+
+            //Render panorama overlay
+            RenderSystem.setShaderTexture(0, overlay);
+            RenderSystem.enableBlend();
+            float f = this.doBackgroundFade ? (float)(Util.getMillis() - this.backgroundFadeStart) / 1000.0f : 1.0f;
+            RenderSystem.setShaderColor(1.0f, 1.0f, 1.0f, this.doBackgroundFade ? (float)Mth.ceil(Mth.clamp(f, 0.0f, 1.0f)) : 1.0f);
+            TitleScreen.blit(matrices, 0, 0, Minecraft.getInstance().getWindow().getGuiScaledWidth(), Minecraft.getInstance().getWindow().getGuiScaledHeight(), 0.0f, 0.0f, 16, 128, 16, 128);
+            RenderSystem.setShaderColor(1.0f, 1.0f, 1.0f, 1.0f);
+        }
     }
 
     public void update(PanoramaRenderer renderer, ResourceLocation panoramaOverlay, boolean doBackgroundFade, long backgroundFadeStart) {
