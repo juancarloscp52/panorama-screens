763080450ba78830fe834253e21dbe4e05ba2ae9
jc <juancarloscp52@gmail.com>
2023-04-22 20:45:57 +0200

    1.19

--- common/src/main/java/me/juancarloscp52/panorama_screen/SettingsGUI.java
+++ common/src/main/java/me/juancarloscp52/panorama_screen/SettingsGUI.java
@@ -4,7 +4,7 @@ import me.shedaniel.clothconfig2.api.ConfigBuilder;
 import me.shedaniel.clothconfig2.api.ConfigCategory;
 import me.shedaniel.clothconfig2.api.ConfigEntryBuilder;
 import net.minecraft.client.gui.screens.Screen;
-import net.minecraft.network.chat.TranslatableComponent;
+import net.minecraft.network.chat.Component;
 
 
 public class SettingsGUI {
@@ -12,17 +12,17 @@ public class SettingsGUI {
     Settings settings;
 
     public Screen getConfigScreen(Screen parent, boolean isTransparent){
-        ConfigBuilder builder = ConfigBuilder.create().setParentScreen(parent).setTitle(new TranslatableComponent("panoramaScreens.options.settings"));
+        ConfigBuilder builder = ConfigBuilder.create().setParentScreen(parent).setTitle(Component.translatable("panoramaScreens.options.settings"));
         builder.setSavingRunnable(PanoramaScreens::saveSettings);
 
         settings = PanoramaScreens.settings;
 
-        ConfigCategory general = builder.getOrCreateCategory(new TranslatableComponent("panoramaScreens.options.settings.general"));
+        ConfigCategory general = builder.getOrCreateCategory(Component.translatable("panoramaScreens.options.settings.general"));
 
         ConfigEntryBuilder entryBuilder = builder.entryBuilder();
-        general.addEntry(entryBuilder.startBooleanToggle(new TranslatableComponent("panoramaScreens.options.settings.general.printScreens"),settings.printScreenNames).setDefaultValue(false).setSaveConsumer(value -> settings.printScreenNames=value).build());
-        general.addEntry(entryBuilder.startStrList(new TranslatableComponent("panoramaScreens.options.settings.general.allowList"), settings.panoramaAllowList).setDefaultValue(Settings.PANORAMA_ALLOW_LIST).setSaveConsumer(strings -> settings.panoramaAllowList=strings).build());
-        general.addEntry(entryBuilder.startStrList(new TranslatableComponent("panoramaScreens.options.settings.general.blockList"), settings.panoramaBlockList).setDefaultValue(Settings.PANORAMA_BLOCK_LIST).setSaveConsumer(strings -> settings.panoramaBlockList=strings).build());
+        general.addEntry(entryBuilder.startBooleanToggle(Component.translatable("panoramaScreens.options.settings.general.printScreens"),settings.printScreenNames).setDefaultValue(false).setSaveConsumer(value -> settings.printScreenNames=value).build());
+        general.addEntry(entryBuilder.startStrList(Component.translatable("panoramaScreens.options.settings.general.allowList"), settings.panoramaAllowList).setDefaultValue(Settings.PANORAMA_ALLOW_LIST).setSaveConsumer(strings -> settings.panoramaAllowList=strings).build());
+        general.addEntry(entryBuilder.startStrList(Component.translatable("panoramaScreens.options.settings.general.blockList"), settings.panoramaBlockList).setDefaultValue(Settings.PANORAMA_BLOCK_LIST).setSaveConsumer(strings -> settings.panoramaBlockList=strings).build());
 
         return builder.setTransparentBackground(isTransparent).build();
     }
--- forge/src/main/java/me/juancarloscp52/panorama_screen/forge/PanoramaScreensForge.java
+++ forge/src/main/java/me/juancarloscp52/panorama_screen/forge/PanoramaScreensForge.java
@@ -2,29 +2,35 @@ package me.juancarloscp52.panorama_screen.forge;
 
 import me.juancarloscp52.panorama_screen.PanoramaScreens;
 import me.juancarloscp52.panorama_screen.SettingsGUI;
-import net.minecraft.network.chat.TranslatableComponent;
-import net.minecraftforge.client.ConfigGuiHandler;
+import net.minecraft.network.chat.Component;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.ConfigScreenHandler;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
 @Mod(PanoramaScreens.MOD_ID)
+@Mod.EventBusSubscriber(modid = PanoramaScreens.MOD_ID, value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class PanoramaScreensForge {
     public PanoramaScreensForge() {
         PanoramaScreens.init();
-
         MinecraftForge.EVENT_BUS.register(this);
-        ModLoadingContext.get().registerExtensionPoint(ConfigGuiHandler.ConfigGuiFactory.class,
-                () -> new ConfigGuiHandler.ConfigGuiFactory(
+    }
+
+    @SubscribeEvent
+    public static void init(final FMLClientSetupEvent event) {
+        ModLoadingContext.get().registerExtensionPoint(ConfigScreenHandler.ConfigScreenFactory.class,
+                () -> new ConfigScreenHandler.ConfigScreenFactory(
                         (minecraft, screen) -> {
                             if(!ModList.get().isLoaded("cloth_config"))
-                                return new PanoramaErrorScreen(new TranslatableComponent("panoramaScreens.clothConfigError"),new TranslatableComponent("panoramaScreens.clothConfigError.description1"),new TranslatableComponent("panoramaScreens.clothConfigError.description2"));
+                                return new PanoramaErrorScreen(Component.translatable("panoramaScreens.clothConfigError"),Component.translatable("panoramaScreens.clothConfigError.description1"),Component.translatable("panoramaScreens.clothConfigError.description2"));
 
                             return new SettingsGUI().getConfigScreen(screen,false);
                         })
         );
-
     }
 
 }
