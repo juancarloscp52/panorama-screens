7abccee7ff0a10e70ec72645939da8f4e94655c0
jc <juancarloscp52@gmail.com>
2023-04-23 14:12:01 +0200

    Improve Compatibility

--- fabric/src/main/java/me/juancarloscp52/panorama_screen/fabric/PanoramaScreensFabric.java
+++ fabric/src/main/java/me/juancarloscp52/panorama_screen/fabric/PanoramaScreensFabric.java
@@ -3,13 +3,16 @@ package me.juancarloscp52.panorama_screen.fabric;
 import me.juancarloscp52.panorama_screen.PanoramaScreens;
 import me.juancarloscp52.panorama_screen.SettingsGUI;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.loader.api.FabricLoader;
 
 public class PanoramaScreensFabric implements ClientModInitializer {
 
-    public static SettingsGUI settingsGUI= new SettingsGUI();;
+    public static SettingsGUI settingsGUI= new SettingsGUI();
 
     @Override
     public void onInitializeClient() {
         PanoramaScreens.init();
+        if(FabricLoader.getInstance().isModLoaded("languagereload"))
+            PanoramaScreens.isLanguageReloadLoaded = true;
     }
 }
